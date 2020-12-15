02ce440bbaed21a040cd6fe8b7ac01c28f839e22
joseph-mpo-yeti <josephmpo@hotmail.com>
2020-12-14 20:13:40 -0500

    Resume updated

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const menu = document.querySelector(".menu-container");
 
 document.addEventListener("click", event => {
     console.log(event.target.classList);
-    if(event.target.classList.contains("menu-title") || event.target.parentNode.classList.contains("menu-title")){
+    if(event.target.classList.contains("menu-title") || event.target.parentNode.classList.contains("menu-title")
+    ){
         menu.classList.toggle("hidden");
-    } else {
+    } else if ( !event.target.classList.contains("menu-list") && 
+        !event.target.parentNode.classList.contains("menu-list") &&
+        !event.target.classList.contains("menu-container") &&
+        !event.target.classList.contains("menu-link")) {
+
         menu.classList.add("hidden");
     }
 });
